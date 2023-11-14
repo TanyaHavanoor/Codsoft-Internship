79f46de0d421bad6949bb6d0fcca422f606a7461
TanyaHavanoor <150812036+TanyaHavanoor@users.noreply.github.com>
2023-11-14 20:34:40 +0530

    Codsoft_Calculator.py

--- Codsoft_Calculator.py
+++ Codsoft_Calculator.py
@@ -1,3 +1,4 @@
+#SIMPLE CALCULATOR
 
 def operation(a,b):
     #function evaluates the expression using 'eval' 
@@ -6,8 +7,24 @@ def operation(a,b):
     final_res=eval(res)  #evaluates the expression and gives the result
     return final_res
 
-print("*******************************************************")
-a=input("Enter a number: ")
-b=input("Enter another number: ")
-result=operation(a,b)  #calling function
-print("The result is",result)
+
+#__MAIN__
+
+print()
+print("*********************************MENU************************************")
+while True:
+    print()
+    ch=input("Enter 'y' to evaluate arithmetic expression or 'n' to stop.\nEnter your choice: ")
+    if ch==('y' or 'Y'):
+        a=input("Enter a number: ")
+        b=input("Enter another number: ")
+        result=operation(a,b)  #calling function
+        print("The result is",result)
+
+    elif ch==('n' or  'N'):
+        print("Exiting!")
+        break
+    else:
+        print("Error! Please enter valid input")
+        print()
+        
